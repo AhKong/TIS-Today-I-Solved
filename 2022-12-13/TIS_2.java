d83b73893edc17cd41d344388714cf5321fcdb0f
Ahkong <40798160+AhKong@users.noreply.github.com>
2022-12-13 10:35:22 +0900

    Programmers -  약수 구하기
    https://school.programmers.co.kr/learn/courses/30/lessons/120897

---
+++ 2022-12-13/TIS_2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+/*
+정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+24	[1, 2, 3, 4, 6, 8, 12, 24]
+*/
+public class Solution {
+    public int[] solution(int n) {
+        List<Integer> integerList = new ArrayList<>();
+        for(int i = 1 ; i<=n;i++){
+            if(n%i==0){
+                integerList.add(i);
+            }
+        }
+        return  integerList.stream().mapToInt(i->i).toArray();
+    }
+}
